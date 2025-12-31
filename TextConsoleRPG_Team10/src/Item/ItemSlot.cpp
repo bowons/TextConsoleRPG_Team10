b35627cb8780482f2c5352b666c5db2dce27f232
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2025-12-31 19:55:19 +0900

    아이템 슬롯 - 코드 규칙 적용, 고정 인벤토리에 따른 규칙 수정

--- TextConsoleRPG_Team10/src/Item/ItemSlot.cpp
+++ TextConsoleRPG_Team10/src/Item/ItemSlot.cpp
@@ -2,39 +2,37 @@
 #include "../../include/Item/IItem.h"
 
 //아이템 슬롯이 비어있는지 확인
+//아이템 개수가 0 이하이면 빈 슬롯으로 간주
 bool ItemSlot::IsEmpty() const {
-    if (_Item == nullptr) {
-        return true;
-    }
-    return false;
+    return _Amount <= 0;
 }
 
 //아이템을 슬롯에 넣음
 //실제 추가는 Inventory 클래스에서 처리
-bool ItemSlot::SetItem(IItem* Item, int amount) {
-    if (_Item != nullptr || Item == nullptr || amount <= 0) {
+bool ItemSlot::SetItem(IItem* Item, int Amount) {
+    if (Item == nullptr || Amount < 0) {
         return false;
     }
 
     _Item = Item;
-    _Amount = amount;
+    _Amount = Amount;
     return true;
 }
 
 //슬롯에 든 아이템 개수 증가
-void ItemSlot::AddAmount(int amount) {
-    if (amount <= 0) {
+void ItemSlot::AddAmount(int Amount) {
+    if (Amount <= 0) {
         return;
     }
-    _Amount += amount;
+    _Amount += Amount;
 }
 
 //슬롯에 든 아이템 개수 감소
-void ItemSlot::RemoveAmount(int amount) {
-    if (amount <= 0) {
+void ItemSlot::RemoveAmount(int Amount) {
+    if (Amount <= 0) {
         return;
     }
-    _Amount -= amount;
+    _Amount -= Amount;
 
     if (_Amount < 0) {
         _Amount = 0;
@@ -53,6 +51,5 @@ IItem* ItemSlot::GetItem() const {
 
 //아이템 슬롯 초기화
 void ItemSlot::ClearItem() {
-    _Item = nullptr;
     _Amount = 0;
 }
