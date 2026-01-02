5eed8c35e02df192941f01d779eda90aaac91b89
yoonseo <cysyes0403@naver.com>
2026-01-02 12:20:36 +0900

    add: 보스 클래스

---
+++ TextConsoleRPG_Team10/include/Unit/Boss.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "IMonster.h"
+#include <tuple>
+#include <memory>
+
+class Boss : public IMonster
+{
+public:
+    Boss(const int PlayerLevel, string Stage, string Name);
+    void TakeDamage(int amount) override;
+    void Attack(ICharacter* target) const override;
+    bool IsDead() const override;
+    tuple<int, int, unique_ptr<IItem>> DropReward() override;
+};
